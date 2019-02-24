f8acb93028d640a1865e082fd244eed9480015bd
Soham Dasgupta <sohamd@hotmail.com>
2019-02-24 01:00:45 -0600

    added random distribution collector / plot

--- change_point_detection.py
+++ change_point_detection.py
@@ -1,8 +1,13 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import math
+import pandas as pd
+from collections import deque
+import copy
+import random
 
-# - Distribution Generator code - #
-# - Code taken from Microsoft speaker (Netflix guy) - #
+# - start generator code - #
 
 class Generator(object):
     
@@ -27,6 +32,7 @@ class DistributionGenerator(Generator):
     def __init__(self, dist, **kwargs):
         self._dist = dist
         self._args = kwargs
+        self._changepoint = 0
         
     def get(self):
         return self._dist.rvs(**self._args)
@@ -118,42 +124,38 @@ class DataBackedGenerator(Generator):
         if self._idx < len(self._vec):
             self._idx += 1
             return self._vec[self._idx - 1]
-        
-class DriftingDistributionGenerator(Generator):
-    """
-    A generator which takes two distinct distributions and a changepoint and returns
-    random variates from the first distribution until it has reached the changepoint
-    where it linearly drifts to the second distribution.
-    
-    dist1:       A scipy.stats distribution for before changepoint.
-    kwargs1:     A map specifying loc and scale for dist1.
-    dist2:       A scipy.stats distribution for after changepoint.
-    kwargs2:     A map specifying loc and scale for dist2.
-    changepoint: The number of values to be generated before switching to dist2.
-    steps:       The number of steps to take in the drift.
-    """
-    
-    _position = 0
-    
-    def __init__(self, dist1, kwargs1, dist2, kwargs2, changepoint, steps):
-        self._dist1 = dist1
-        self._kwargs1 = kwargs1
-        self._dist2 = dist2
-        self._kwargs2 = kwargs2
-        self._changepoint = changepoint
-        self._steps = steps
-        
-        self._change_gradient = np.linspace(0, 1, self._steps)
-        
-    def get(self):
-        self._position += 1
-        if self._position <= self._changepoint:
-            return self._dist1.rvs(**self._kwargs1)
-        elif self._position > self._changepoint and self._position <= self._changepoint + self._steps:
-            beta = self._change_gradient[self._position - self._changepoint - 1]
-            return ((1.0 - beta) * self._dist1.rvs(**self._kwargs1)) + (beta * self._dist2.rvs(**self._kwargs2))
-        else:
-            return self._dist2.rvs(**self._kwargs2)
 
-# - End generator code - #
+# - end generator code - #
 
+
+# make different distributions with randomized parameters for the mean (loc) and standard deviation (scale)
+
+distributions = []
+
+def gen_values(gen):
+    
+    vals = np.zeros(100)
+    for x in range(100):
+        vals[x] = gen.get()
+
+    distributions.append((vals,gen._changepoint))
+
+# loc = mean of distribution
+# scale = standard deviation
+
+gen_values(DistributionGenerator(stats.norm, **{'loc': random.randint(5,25), 'scale': random.randint(1,4)}))
+gen_values(ChangingDistributionGenerator(stats.norm, {'loc': random.randint(5,25), 'scale': random.randint(1,4)},stats.norm, {'loc': random.randint(5,25), 'scale': random.randint(1,4)}, random.randint(30,70)))
+gen_values(DriftGenerator(stats.norm, {'loc': random.randint(5,25), 'scale': random.randint(1,4)},stats.norm, {'loc': random.randint(5,25), 'scale': random.randint(1,4)}, random.randint(30,70), 5))
+
+# TODO: iterate through existing distributions and try different CPD methods on each one
+# TODO: decide on metric(s) for assessing general performance of a CPD method
+# TODO: calculate an aggregate score which represents the effectiveness of a CPD method across the different distributions, derived from the above metric
+# TODO: draw insights: are some methods better on certain distributions, etc...
+
+for dist in distributions:
+    plt.plot(dist[0])
+    plt.axvline(x= dist[1],**{'color': 'red'})
+    plt.show()
+
+"""questions for the team"""
+# are we worried more about false positives or false negatives
